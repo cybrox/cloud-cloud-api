9742574322c70257d8691879bb38cfa889ab768d
Sven Gehring <cbrxde@gmail.com>
2017-07-04 08:44:39 +0200

    Add proper socket retry implementation to dispatcher

--- lib/cloud/dispatcher.ex
+++ lib/cloud/dispatcher.ex
@@ -9,40 +9,50 @@ defmodule Cloud.Dispatcher do
   (or on user interaciton) send it respective state updates.
   """
 
+  @default_state %{server: nil, client: nil, open: false}
+
   def start_link do
-    GenServer.start_link(__MODULE__, %{server: nil, client: nil}, name: __MODULE__)
+    GenServer.start_link(__MODULE__, @default_state, name: __MODULE__)
   end
 
 
   def init(state) do
-    Logger.info "Starting websocket dispatcher"
-    Process.send(self(), :await_connect, [:nosuspend])
-
-    {:ok, state}
-  end
-
-  def handle_info(:await_connect, _state) do
     wsport = Application.get_env(:cloud, :device_port)
     Logger.info "Awaiting connection on wss:#{wsport}"
 
     server = Socket.Web.listen!(wsport)
-    client = Socket.Web.accept!(server)
+    Process.send(self(), :await_connect, [:nosuspend])
+
+    {:ok, %{state | server: server}}
+  end
+
+  def handle_info(:await_connect, state) do
+    client = Socket.Web.accept!(state.server)
     client |> Socket.Web.accept!()
 
     Process.send(self(), :await_auth, [:nosuspend])
-    {:noreply, %{server: server, client: client}}
+    {:noreply, %{state | client: client}}
   end
 
   def handle_info(:await_auth, state) do
     auth_secret = Application.get_env(:cloud, :auth_secret)
     case Socket.Web.recv!(state.client) do
-      {:text, ^auth_secret} -> Socket.Web.send!(state.client, {:text, "ok"})
-      _ ->
-        IO.inspect state.server
-        Socket.Web.close(state.client, :normal)
-        Process.send(self(), :await_connect, [:nosuspend])
-    end
+      {:text, ^auth_secret} ->
+        Socket.Web.send!(state.client, {:text, "ok"})
+        {:noreply, %{state | open: true}}
 
-    {:noreply, state}
+      _ ->
+        Socket.Web.close(state.client, :handshake)
+        Process.send(self(), :await_connect, [:nosuspend])
+        {:noreply, %{state | open: false}}
+    end
+  end
+
+  def handle_call({:send_display, display}, _from, state) do
+    with true <- state.open do
+      {:reply, :ok, state}
+    else
+      {:reply, :notopen, state}
+    end
   end
 end
