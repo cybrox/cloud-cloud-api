102690ba4db1d5d2883f6b6d422c075b8333c181
Sven Gehring <cbrxde@gmail.com>
2017-07-04 08:31:59 +0200

    Add port and auth config, add logger config

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,11 @@
 use Mix.Config
 
 config :cloud,
+  client_port: 6660,
+  device_port: 6661,
+  auth_secret: "cloud-my-butt",
   api_key: "d3eca8838531a07f8d5efd88f4a07d45"
+
+config :logger, 
+  backends: [:console],
+  compile_time_purge_level: :debug
