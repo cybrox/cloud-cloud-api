3a46eba6b8382b0100d34b16f02269c31be863d2
Sven Gehring <cbrxde@gmail.com>
2017-07-06 16:02:17 +0200

    Provide new, updated default configuration

--- config/cloud.exs
+++ config/cloud.exs
@@ -1,6 +1,9 @@
 # Cloud-cloud API configuration
+use Mix.Config
+
 config :cloud,
-  client_port: 0,
-  device_port: 0,
-  auth_secret: "",
-  api_key: ""
+  client_port: 80,
+  device_port: 8080,
+  city_id: "000000",
+  auth_secret: "myauthtoken",
+  api_key: "myopenweathermapkey"
